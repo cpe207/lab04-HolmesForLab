5c362806c9624798dec6a6ba30761533e4df9743
HolmesForLab <mtbholmes2@gmail.com>
2024-07-02 16:54:44 +0700

    Update q3.js

--- q3.js
+++ q3.js
@@ -1,11 +1,9 @@
 function primeNumber(a) {
-    if (a <= 1)
-        return false;
-    for (var i = 2; i <= Math.sqrt(a); i++) {
-        if (a % i === 0)
-            return false;
-    }
-    return true;
+  if (a <= 1) return "NO";
+  for (let i = 2; i <= Math.sqrt(a); i++) {
+      if (a % i === 0) return "NO";
+  }
+  return "YES";
 }
 var d1 = 10;
 var d2 = 29;
